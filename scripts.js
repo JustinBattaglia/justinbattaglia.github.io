5326449bd325857fdecb76349be311f80c25be28
JustinBattaglia <JustinBattaglia@users.noreply.github.com>
2021-07-21 21:56:40 -0500

    new

---
+++ scripts.js
@@ -0,0 +1,15 @@
+//var navbar = document.querySelector('nav').style.backgroundColor = "#00d4ff";
+
+var navbar = document.querySelector('nav');
+
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 20 || document.documentElement.scrollTop >= 20 ) {
+        navbar.classList.add("nav-colored");
+        navbar.classList.remove("nav-transparent");
+    } 
+    else {
+        navbar.classList.add("nav-transparent");
+        navbar.classList.remove("nav-colored");
+    }
+};
