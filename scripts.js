befab93b2f4d62c25bae53b56b2377d8abb03893
JustinBattaglia <justinbattagliajpro@gmail.com>
2022-08-08 20:40:40 -0500

    update

--- scripts.js
+++ scripts.js
@@ -44,67 +44,6 @@ $('a[href*="#"]')
     }
   });
 
-var $elem = $('.html');
-var $elem2 = $('.css');
-var $elem3 = $('.php');
-var $elem4 = $('.javascript');
-var $elem5 = $('.wordpress');
-var in_view = new Waypoint.Inview({
-    element: $elem5[0],
-    element: $elem4[0],
-    element: $elem3[0],
-    element: $elem2[0],
-    element: $elem[0],
-    enter: function() {
-        $elem.addClass('start-html');
-        $elem2.addClass('start-css');
-        $elem3.addClass('start-php');
-        $elem4.addClass('start-javascript');
-        $elem5.addClass('start-wordpress');
-    }
-});
-
-// SLIDESHOW
-var arrowF = document.getElementById('arrowf');
-
-var slide0 = document.getElementById('slide0');
-var slide1 = document.getElementById('slide1');
-var slide2 = document.getElementById('slide2');
-const maxSlide = 3;
-let slideArray = [slide0,slide1,slide2];
-
-slide0.style.display = "block";
-slide1.style.display = "none";
-slide2.style.display = "none";
-
-var i = 1;
-
-function movef() {
-    slide0.style.display = "none";
-    slide1.style.display = "none";
-    slide2.style.display = "none";
-    slideArray[i].style.display = "block";
-    console.log(i);
-    if (i != maxSlide-1) {
-        i = i+1;
-    } else {
-        i = 0;
-    }
-    
-}    
-
-function moveb() {
-    slide0.style.display = "none";
-    slide1.style.display = "none";
-    slide2.style.display = "none";
-    slideArray[i].style.display = "block";
-    if (i != 0) {
-        i -= 1;
-    } else {
-        i = 2
-    }
-    
-} 
 
 
 function moveHam() {
